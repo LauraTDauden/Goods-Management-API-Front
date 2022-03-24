f778db8ac7c22e19896b44ad0bcae450026f5c26
LauraTDauden <lauratdauden@gmail.com>
2022-03-24 14:46:25 +0000

    togglebutton and itemcontext

---
+++ src/components/items/ItemCard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../assets/iteminfo.css';
+
+export const ItemCard = ({
+    id,
+    code,
+    description,
+    price,
+    state,
+    suppliers,
+    price_reductions,
+    creation_date,
+    creator
+}) => {
+
+    return (
+        <div className="col">
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-text">Item Code: {code}</h5>
+                    <p className="card-text">Description: {description}</p>
+                    <hr/>
+                    <p className="card-text">State: {state}</p>
+                    <p className="card-text">Price: {price.toFixed(2)} €</p>
+                    <p className="card-text">{creation_date}</p>
+                    <p className="card-text">{creator.username}</p>
+                    <Link to={`/products/${id}`}
+                    item ={item} >
+                        More information
+                            </Link>
+                </div>
+            </div>
+        </div>
+
+    )
+}
--- src/components/items/ItemComponent.js
+++ src/components/items/ItemComponent.js
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { ItemContext, ItemDispatchContext } from '../../context/ItemContext';
 import { useFetchItems } from '../../hooks/useFetchItems';
+import { ToggleState } from '../utils/ToggleState';
+import { ItemCard } from './ItemCard';
 
 
 export const ItemComponent = () => {
 
     const {data: items, loading} = useFetchItems();
 
+    const [selected, setSelected] = useState(true);
+
+    const setItemList = useContext(ItemDispatchContext);
+    const itemList = useContext(ItemContext);
+    useEffect(() => {
+        setItemList(items);
+    })
+
+    const toggle = () => {
+        setSelected(!selected);
+    }
+
     return (
         <div>
+            <ToggleState
+                selected={selected}
+                toggle={toggle}
+            />
             {loading && <p>Loading...</p>}
+            {/*console.log(itemList)*/}
             {
                 items.map(item=>(
-                    <div
-                    key = {item.id}>
-                        <h4>{item.code}</h4>
-                        <p>{item.description}</p>
-
-                    </div>
+                    <ItemCard
+                    key = {item.id}
+                    {...item}
+                    />
+                   
                 ))
             }
             
